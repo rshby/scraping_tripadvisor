2ad2ea95eb3e254d97dbd3afdee2affac4e2c801
Reo Sahobby <60124728+rshby@users.noreply.github.com>
2021-06-20 13:27:26 +0700

    update_save csv

--- scraping1.py
+++ scraping1.py
@@ -14,9 +14,25 @@ url = "https://www.tripadvisor.com/Hotels-g294230-Yogyakarta_Region_Java-Hotels.
 
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
+data_hotel = []
 
 for item in soup.findAll("div", "ui_column is-8 main_col allowEllipsis"):
-    nama = item.find("a", "property_title prominent").text.replace("@", "")
+    nama = item.find("a", "property_title prominent").text.replace(
+        "@", "").strip()
     harga = item.find(
-        "div", {"data-sizegroup": "mini-meta-price"}).text.replace("IDR", "Rp").replace(",", ".")
-    print(f"{nama}, {harga}\n")
+        "div", {"data-sizegroup": "mini-meta-price"}).text.replace("IDR", "").replace(",", "")
+    jumlah_review = item.find("a", "review_count").text.replace(
+        "reviews", "").replace("review", "")
+    #print(f"Nama Hotel: {nama}\nHarga: {harga}\nReview: {jumlah_review}\n")
+    data_hotel.append([nama, harga, jumlah_review])
+    print(f"Harga: {harga}\n")
+
+# save to csv
+kolom = ["Nama", "Alamat", "Review"]
+writer = csv.writer(open("tripAd.csv", "w", newline=""))
+writer.writerow(kolom)
+
+for d in data_hotel:
+    writer.writerow(d)
+
+print(data_hotel)
