40fbbb1fc4b56dd47fb821ecef7d36852b673f00
Reo Sahobby <60124728+rshby@users.noreply.github.com>
2021-08-02 10:20:46 +0700

    Update tripAdv_hotel.py

--- tripAdv_hotel.py
+++ tripAdv_hotel.py
@@ -1,29 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
+import requests  # import library yang digunakan
+from bs4 import BeautifulSoup  # import library yang digunakan
+import csv  # import library yang digunakan untuk menyimpan file ke csv
 
+# url yang akan discraping
 url = "https://www.tripadvisor.com/Hotels-g294230-Yogyakarta_Region_Java-Hotels.html"
 
-req = requests.get(url)
+req = requests.get(url)  # buat variabel req untuk load url
+# buat variabel soup untuk menggunakan BeautifulSoup pada url yang akan discraping
 soup = BeautifulSoup(req.text, "html.parser")
-data_hotel = []
+data_hotel = []  # buat list kosong untuk menampung data hotel
 
+# buat perulangan untuk mengambil elemen yang akan discraping
 for item in soup.findAll("div", "ui_column is-8 main_col allowEllipsis"):
     nama = item.find("a", "property_title prominent").text.replace(
-        "@", "").strip()
-    harga = item.find(
-        "div", {"data-sizegroup": "mini-meta-price"}).text.replace("IDR", "").replace(",", "")
-    jumlah_review = item.find("a", "review_count").text.replace(
-        "reviews", "").replace("review", "")
+        "@", "").strip()  # scrap/ambil elemen yang berisi informasi nama hotel
+    harga = item.find("div", {"data-sizegroup": "mini-meta-price"}).text.replace(
+        "IDR", "").replace(",", "")  # ambil elemen yang berisi informasi harga hotel
+    jumlah_review = item.find("a", "review_count").text.replace("reviews", "").replace(
+        "review", "")  # ambil elemen yang berisi informasi jumlah review
+
+    # masukkan data yang discrap ke list variabel data_hotel
     data_hotel.append([nama, harga, jumlah_review])
-    print(f"Harga: {harga}\n")
 
 # save to csv
-kolom = ["Nama", "Harga", "Review"]
+kolom = ["Nama", "Harga", "Review"]  # buat nama kolom
+# buat file csv (nama: tripAd.csv)
 writer = csv.writer(open("tripAd.csv", "w", newline=""))
-writer.writerow(kolom)
+writer.writerow(kolom)  # untuk membuat kolom dan nama kolom dalam file csv
 
-for d in data_hotel:
+for d in data_hotel:  # buat perulangan untuk mengambil data di dalam list data_hotel
+    # masukkan data yang ada di dalam data_hotel ke dalam file csv yang sudah dibuat
     writer.writerow(d)
 
-print(data_hotel)
+print(data_hotel)  # tampilkan data yang sudah discraping
