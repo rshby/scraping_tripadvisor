f4d1071d5b01a8e3ac7bfd26ad78a58e8a916ca4
Reo Sahobby <60124728+rshby@users.noreply.github.com>
2021-08-03 16:18:01 +0700

    Update codingan scraping pegi-pegi

--- scraping_pegi.py
+++ scraping_pegi.py
@@ -7,7 +7,7 @@ data_pegi = []  # buat variabel untuk menampung data hasil scraping
 
 # karena hotel di jogja pada halaman pegi-pegi ada 39 pages
 # maka buat perulangan untuk scraping pages 1 sampai pages 39
-for i in range(1, 39):
+for i in range(1, 2):
     url = "https://www.pegipegi.com/hotel/jogja/" + \
         str(i)+".html"  # url masing-masing pages
     req = requests.get(url)
@@ -17,6 +17,7 @@ for i in range(1, 39):
 
     for hotel in link_hotel:  # scrap data masing-masing hotel
         # url hotel adalah href dari hasil scraping elemen link_hotel
+        tersedia_kamar = 0
         url_hotel = hotel["href"]
         req_hotel = requests.get(url_hotel)
         soup1 = BeautifulSoup(req_hotel.text, "html.parser")
@@ -33,13 +34,22 @@ for i in range(1, 39):
                 "img", {"class": "slideShowRoom gm-img-center"})
         except:
             kamar = "0"  # apabila kamar hotel tidak ditemukan
-        jumlh_kamar = 1
+        jumlh_kamar = 0
         for item in kamar:
             jumlh_kamar += 1  # hitung semua jumlah kamar yang ada
+
+        for ktersedia in soup1.findAll("div", {"class": "listContentRoom"}):
+            kmr_tsd = soup1.find("div", {"class": "note"}).text.replace(
+                "Tersedia", "").replace("kamar lagi !", "").strip()
+            kmr_tsd = 0 if kmr_tsd == "Kamar terbatas !" else kmr_tsd
+            tersedia_kamar += int(kmr_tsd)
         # masukkan data hasil scraping ke variabel data_pegi
-        data_pegi.append([nama_hotel, alamat, jumlh_kamar])
+        total_kamar = jumlh_kamar+tersedia_kamar
+        print(
+            f"nama hotel : {nama_hotel}\njenis kamar : {jumlh_kamar}\nkamar tersedia : {tersedia_kamar}\ntotal tersedia : {total_kamar}\n")
+        # data_pegi.append([nama_hotel, alamat, jumlh_kamar])
 
-
+"""
 # siapkan nama kolom untuk data csv
 kolom = ["nama_hotel", "alamat_hotel", "jumlah_kamar"]
 writer = csv.writer(open("hotel_pegi.csv", "w", newline="")
@@ -48,3 +58,5 @@ writer.writerow(kolom)
 
 for d in data_pegi:
     writer.writerow(d)  # masukkan data ke file csv yang sudah dibuat
+"""
+print(data_pegi)
